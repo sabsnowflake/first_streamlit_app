ca38714eae01b9d4585804bc1ddb785efe330224
sabsnowflake <106002572+sabsnowflake@users.noreply.github.com>
2022-05-24 22:14:01 +1000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -38,7 +38,7 @@ except URLError as e:
 streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
-def get _fruit_load_list():
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
