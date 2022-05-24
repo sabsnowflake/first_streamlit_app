a88efb56a3d75e608812f039cdcbc82964715155
sabsnowflake <106002572+sabsnowflake@users.noreply.github.com>
2022-05-24 15:51:46 +1000

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -21,3 +21,9 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" +fruit_choi
 fruityvice_normalized= pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("Select Current_user(), current_account(), current_region()")
+my_data_row= my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+stremalit.text(my_data_row)
